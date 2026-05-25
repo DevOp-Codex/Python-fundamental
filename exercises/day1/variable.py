eadffaab8c0ce92c53c5d2b5ee4b072a5973a0a8
DevOp-Codex <codexharry11@email.com>
2026-05-25 12:53:04 +0000

    learning variable

---
+++ exercises/day1/variable.py
@@ -0,0 +1,15 @@
+digit = 45
+name = "Codex"
+height = 17.5
+is_rainy = True
+
+print(name)
+print(height)
+print(digit)
+print()
+# Multiple Declaration of variable
+age, location, weight, eligible = 22, "Greater Accra", 67.91, False 
+print(age)
+print(location)
+print(weight)
+print(eligible)
