3be872f6f3a8be658ceaf6d92f41798ed4fad2f7
DevOp-Codex <codexharry11@email.com>
2026-05-25 13:03:43 +0000

    little update

--- exercises/day1/variable.py
+++ exercises/day1/variable.py
@@ -7,7 +7,9 @@ print(name)
 print(height)
 print(digit)
 print()
+
 # Multiple Declaration of variable
+
 age, location, weight, eligible = 22, "Greater Accra", 67.91, False 
 print(age)
 print(location)
