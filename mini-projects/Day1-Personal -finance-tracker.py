400e037a691693c9e023477cd806bc84bbfebabe
DevOp-Codex <codexharry11@email.com>
2026-05-26 14:28:27 +0000

    final commit for day1

--- mini-projects/Day1-Personal -finance-tracker.py
+++ mini-projects/Day1-Personal -finance-tracker.py
@@ -6,15 +6,15 @@ print()
 name = input ("What's your name?: ")
 print(" Okay!!")
 print()
-income = float (input ("Enter the total income for next month: "))
+income = float (input ("Enter the total income for next month: $"))
 print(" Okay!!")
-transport = float ( input ("What's the total expenses for transportation?: "))
+transport = float ( input ("What's the total expenses for transportation?: $"))
 print(" Good!!")
-feeding = float(input ("What's the total expenses for nutrition?: "))
+feeding = float(input ("What's the total expenses for nutrition?: $"))
 print(" Alright")
-internet = float ( input (" How much will browsing & data cost?: "))
+internet = float ( input (" How much will browsing & data cost?: $"))
 print(" Good!! ")
-saving = float (input ("What is your saving target?: "))
+saving = float (input ("What is your saving target?: $"))
 print("perfect!!")
 
 total_expenses = transport + feeding + internet
@@ -24,11 +24,11 @@ remainder = balance - saving
 
 print("====== Final-track-down =======")
 print()
-print("Dear Mr. " + name +"Your monthly salary is " +income)
-print("Your total amount for expenses in the upcoming month is " +total_expenses)
-print("After calculation, the balance is "+ balance )
-print("The percentage of saving is " + saving_percentage)
-print("The residual money from ur income is " + remainder)
+print("Dear Mr. " ,name , "Your monthly salary is $",income )
+print("Your total amount for expenses in the upcoming month is $" ,total_expenses)
+print("After calculation, the balance is $", balance )
+print("The percentage of saving is " , saving_percentage,"%")
+print("The residual money from ur income is $" , remainder)
 
 
 
